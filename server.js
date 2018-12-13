c45a84185a0ced3abb66ae6a0f2c0e666a1ac8f7
Todd <todd.mcbrearty@gmail.com>
2018-12-13 12:35:12 -0500

    update server

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const serveStatic = require('serve-static');
 let app = express();
 app.use(serveStatic(__dirname + '/dist'));
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
-});
+const port = process.env.PORT || 3000;
+app
+  .listen(port, () => {
+    console.log('Listening on port ' + port);
+  })
+  .catch(error => console.log(error));
