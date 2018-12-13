90786d0655f02249e268de908893fb04f9b2f998
Todd <todd.mcbrearty@gmail.com>
2018-12-13 15:06:44 -0500

    prod server

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8080;
-const publicDir = __dirname + '/../dist';
 
-// set the home page route
-app.get('/', function(req, res) {
-  res.sendFile(publicDir + '/index.html');
+// the __dirname is the current directory from where the script is running
+const publicPath = path.join(__dirname, 'dist');
+const indexPath = path.join(publicPath, 'index.html');
+
+app.use(express.static(publicPath));
+
+// send the user to index html page inspite of the url
+app.get('/', (req, res) => {
+  res.sendFile(indexPath);
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log('Our app is running on http://localhost:' + port);
 });
