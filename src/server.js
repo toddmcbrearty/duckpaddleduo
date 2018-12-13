e8155e58f843a6c2061b4d1a8f4dd63b3994bde3
Todd <todd.mcbrearty@gmail.com>
2018-12-13 13:13:55 -0500

    updated server

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 const host = '0.0.0.0';
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/../dist'));
 
 // set the home page route
 app.get('/', function(req, res) {
