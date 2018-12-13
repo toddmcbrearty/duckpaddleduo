168742ea723751e1ce1cf90e6c71b62e583abd00
Todd <todd.mcbrearty@gmail.com>
2018-12-13 14:08:00 -0500

    updated package

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const publicDir = __dirname + '/../dist';
 
 // set the home page route
 app.get('/', function(req, res) {
-  res.sendFile('index.html');
+  res.sendFile('./index.html');
 });
 
 app.listen(port, function() {
