37d56b0ddccd75aa5dc2012b91ce1d0e6b2c8159
Todd <todd.mcbrearty@gmail.com>
2018-12-13 12:41:39 -0500

    removed catch

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ let app = express();
 app.use(serveStatic(__dirname + '/dist'));
 
 const port = process.env.PORT || 3000;
-app
-  .listen(port, () => {
-    console.log('Listening on port ' + port);
-  })
-  .catch(error => console.log(error));
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
