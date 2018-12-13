106fc28118d8be1e1e5d67886ed99e78e65c3638
Todd <todd.mcbrearty@gmail.com>
2018-12-13 14:03:44 -0500

    updated package

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const app = express();
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8080;
-const host = '0.0.0.0';
 const publicDir = __dirname + '/../dist';
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(publicDir));
 
 // set the home page route
 app.get('/', function(req, res) {
-  // ejs render automatically looks in the views folder
   res.sendFile(publicDir + '/index.html');
 });
 
